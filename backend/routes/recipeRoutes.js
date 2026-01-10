2c0480bae0ffc49b4e26a1dbcf1e38f959403da0
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-10 23:38:23 +0000

    Fix route filename case for Linux

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Recipe = require("../models/recipe");
+const Recipe = require("../models/Recipe");
 const auth = require("../middleware/user");
 
 // PUBLIC – get all recipes
