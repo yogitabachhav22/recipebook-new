1a9a318c122b3f9382bffd457cbd2fd65fe7f212
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-10 23:50:45 +0000

    Temp rename to fix case conflict

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Recipe = require("../models/Recipe");
+const Recipe = require("../models/recipe_temp");
 const auth = require("../middleware/user");
 
 // PUBLIC – get all recipes
