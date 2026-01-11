28284670b8b1820e2a5501847657c6846a77aa39
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-11 00:23:25 +0000

    Temp rename user model to fix casing issue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/user_temp');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
