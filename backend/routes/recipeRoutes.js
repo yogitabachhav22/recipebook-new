3f209cbd2866dfa151335eb03b83e8e460867b64
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-11 00:14:11 +0000

    Fix case-sensitive filename issue

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Recipe = require("../models/recipe");
-const auth = require("../middleware/auth_temp");
+const auth = require("../middleware/auth");
 
 // PUBLIC – get all recipes
 router.get("/", async (req, res) => {
