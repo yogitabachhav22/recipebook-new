ca05b1736df17b2a72f18ae911855d59234c485c
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-11 00:24:31 +0000

    Rename user model correctly

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user_temp');
+const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
