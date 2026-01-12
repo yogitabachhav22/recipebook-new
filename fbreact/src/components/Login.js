18cd76037f459b9fb49b2e7ffb3950398f0881f2
yogitabachhav22 <ybachhav135@gmail.com>
2026-01-12 14:29:19 +0000

    Fix case-sensitive component imports for Render

--- fbreact/src/components/Login.js
+++ fbreact/src/components/Login.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "./auth.css";
+import "./Auth.css";
 
 function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
--- fbreact/src/components/Signup.js
+++ fbreact/src/components/Signup.js
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./auth.css";
+import "./Auth.css";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
